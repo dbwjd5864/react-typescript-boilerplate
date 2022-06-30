5d6521b5a56afac0918ca5d750c0d38f4cb53fad
Yujeong Choung <yujeong.choung@gmail.com>
2022-07-01 00:57:41 +0900

    [React-Typescript-Boilerplate] 리액트 타입스크립트 보일러플레이트 생성해보기 (continue)

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+const App: FunctionComponent = (): JSX.Element => {
+	return <div>this is react typescript boilerplate</div>;
+};
+
+export default App;
